4da592152b361df92b50490524fa514da9f7f3e2
Tobias Savinainen <tcs1000@hotmail.com>
2012-09-19 14:52:08 +0200

    Added the main program

--- S-Expression Interpreter/main.cpp
+++ S-Expression Interpreter/main.cpp
@@ -1,6 +1,56 @@
-#include <iostream>
+////////////////////////////////////////////////////////////////////////////////
+// main.cpp
+////////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <string>
+#include "Source/ExpressionParser.hpp"
+#include "Source/SExpressionParser.hpp"
+#include "Source/Evaluator.hpp"
+
+////////////////////////////////////////////////////////////////////////////////
+// Entry point of application
+////////////////////////////////////////////////////////////////////////////////
 int main()
 {
+	std::cout << "S-Expression Interpreter!" << std::endl << std::endl;
+
+	while(true)
+	{
+		try
+		{
+			// Get input
+			std::cout << "=> ";
+			std::string line;
+			getline(std::cin,line);
+
+			if(line == "quit")
+				break;
+			else if(line.empty())
+				throw std::string("Error: Empty expression");
+			else
+			{
+				// Parse the expression
+				if(line[0] == ':')
+				{	
+					SExpressionParser::print(SExpressionParser::parse(line.substr(1,line.size() - 1)));
+				}
+				else
+				{
+					std::cout << Evaluator::evaluate(ExpressionParser::parse(line));
+				}
+				
+				std::cout << std::endl;
+			}
+		}
+		catch(std::string s)
+		{
+			std::cout << s << std::endl;
+		}
+	}
+
 	return 0;
 }
